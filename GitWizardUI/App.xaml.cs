b65c2f8f4ce19b9e28a3cb6c83ad71b4c2d8c1d8
Matt Schoen <mtschoen@gmail.com>
2022-04-27 21:20:43 -0700

    Add project files.

---
+++ GitWizardUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GitWizardUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
